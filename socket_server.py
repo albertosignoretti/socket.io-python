3a8c0fc191af55ebf8377ed92fe7a25a2a720ebe
OPTI soluções <optiappsbrasil@gmail.com>
2019-03-26 13:19:34 -0300

    Exemplo completo com socket

--- socket_server.py
+++ socket_server.py
@@ -1,5 +1,6 @@
 # socket_server.py
 from aiohttp import web
+from time import sleep
 import socketio
 # Cria um novo servidor Socket asyncrono
 sio = socketio.AsyncServer()
@@ -15,13 +16,24 @@ async def index(request):
 
 # Para criar um endpoint web socket, utilize este
 # decorador, passando o nome do evento que desejamos observar
-@sio.on('message')
-async def print_message(sid, message):
+@sio.on('execute')
+async def execute(sid, dataSet):
     # Nós recebemos uma novo evento chamado ‘message’
 	# por meio de uma conexão socket
     # Escrevendos o Id da conexão e a mensagem enviada
-    print("Socket ID: " , sid)
-    print(message)
+    print("Iniciando a execução por: " , sid)
+    print(dataSet)
+    sleep(2)
+    await sio.emit('progress', 20)
+    sleep(2)
+    await sio.emit('progress', 40)
+    sleep(2)
+    await sio.emit('progress', 60)
+    sleep(2)
+    await sio.emit('progress', 80)
+    sleep(2)
+    await sio.emit('progress', 100)
+
 
 # Agora nós vamos vincular nosso endpoint aiohttp
 # ao roteamento da aplicação
